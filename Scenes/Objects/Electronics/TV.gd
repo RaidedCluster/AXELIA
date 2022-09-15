e95867a225bcd5860d482ac8ce94a92b1b0f1c37
RaidedCluster <johansdaniel2005@gmail.com>
2022-09-15 19:18:46 +0530

    Dialogue Mechanism
    -Added dialogue for a few objects.

---
+++ Scenes/Objects/Electronics/TV.gd
@@ -0,0 +1,21 @@
+extends StaticBody2D
+
+onready var TVInteraction=$InteractionArea
+
+func _ready():
+	connect("body_entered",self,"_on_body_enter")
+	connect("process",self,"_process")
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		var areas=TVInteraction.get_overlapping_areas()
+		for area in areas:
+			if area.get_name()=="Interaction Area":
+				get_tree().paused = true
+				var new_dialog=Dialogic.start('TV')
+				new_dialog.pause_mode=Node.PAUSE_MODE_PROCESS
+				add_child(new_dialog)
+				new_dialog.connect("timeline_end",self,"resume")
+
+func resume(timeline_name):
+	get_tree().paused = false
---
+++ Scenes/Objects/Electronics/TableTablet.gd
@@ -0,0 +1,21 @@
+extends Sprite
+
+onready var TableTabletInteraction=$InteractionArea
+
+func _ready():
+	connect("body_entered",self,"_on_body_enter")
+	connect("process",self,"_process")
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		var areas=TableTabletInteraction.get_overlapping_areas()
+		for area in areas:
+			if area.get_name()=="Interaction Area":
+				get_tree().paused = true
+				var new_dialog=Dialogic.start('TableTablet')
+				new_dialog.pause_mode=Node.PAUSE_MODE_PROCESS
+				add_child(new_dialog)
+				new_dialog.connect("timeline_end",self,"resume")
+
+func resume(timeline_name):
+	get_tree().paused = false
